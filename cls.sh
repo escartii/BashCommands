f8284c6d0684047024be840b9ca6a2c9082ed3a0
Alvaro Escarti <escartismx@gmail.com>
2024-04-14 22:44:03 +0200

    segundo comando

---
+++ cls.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Definir un alias dentro del script
+alias cls='clear'
+
+# Ahora puedes usar el alias dentro del script
+cls
+
+# Sin embargo, el alias no estará disponible fuera de este script
+# Para añadirlo, lee atentamente el README.md
